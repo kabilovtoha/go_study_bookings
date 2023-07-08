e86cfad965b0f66ae916545d661716a34e12660c
kabilovtoha <kabilov2011@gmail.com>
2023-07-08 22:15:39 +0400

    dev 2

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,5 +19,13 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/rooms/generals-quarters", handlers.Repo.RoomsGeneralsQuarters)
+	mux.Get("/rooms/majors-suite", handlers.Repo.RoomsMajorsSuite)
+	mux.Get("/contacts", handlers.Repo.Contacts)
+	mux.Get("/reservation", handlers.Repo.Reservation)
+	mux.Get("/make-reservation", handlers.Repo.MakeReservation)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 	return mux
 }
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -30,8 +30,11 @@ func (repo *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	repo.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
-	render.RenderTemplate(w, "home.page.gohtml", &models.TemplateData{})
+	render.RenderTemplate(w, "index.page.gohtml", &models.TemplateData{})
 }
+
+// /rooms/generals-quarters
+// /rooms/majors-suite
 
 // About is the About page handler
 func (repo *Repository) About(w http.ResponseWriter, r *http.Request) {
@@ -40,6 +43,43 @@ func (repo *Repository) About(w http.ResponseWriter, r *http.Request) {
 	StringMap := map[string]string{"remote_ip": remoteIP}
 
 	render.RenderTemplate(w, "about.page.gohtml", &models.TemplateData{
+		StringMap: StringMap,
+	})
+}
+
+func (repo *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "reservation.page.gohtml", &models.TemplateData{})
+}
+
+func (repo *Repository) MakeReservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.gohtml", &models.TemplateData{})
+}
+
+func (repo *Repository) Contacts(w http.ResponseWriter, r *http.Request) {
+	StringMap := map[string]string{}
+	render.RenderTemplate(w, "contacts.page.gohtml", &models.TemplateData{
+		StringMap: StringMap,
+	})
+}
+
+func (repo *Repository) RoomsGeneralsQuarters(w http.ResponseWriter, r *http.Request) {
+
+	StringMap := map[string]string{
+		"post_header": "General's Quarters",
+		"image":       "/static/images/room-images/generals-quarters.png",
+	}
+
+	render.RenderTemplate(w, "rooms.page.gohtml", &models.TemplateData{
+		StringMap: StringMap,
+	})
+}
+
+func (repo *Repository) RoomsMajorsSuite(w http.ResponseWriter, r *http.Request) {
+	StringMap := map[string]string{
+		"post_header": "Major's Quarters",
+		"image":       "/static/images/room-images/marjors-suite.png",
+	}
+	render.RenderTemplate(w, "rooms.page.gohtml", &models.TemplateData{
 		StringMap: StringMap,
 	})
 }
